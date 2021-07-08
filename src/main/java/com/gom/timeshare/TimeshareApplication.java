f79f27a23e2a83f147041e4bc41a6322b49da725
azimemory <killsky2014@naver.com>
2021-07-08 20:19:34 +0900

    firebase 푸시 구현 시작

--- src/main/java/com/gom/timeshare/TimeshareApplication.java
+++ src/main/java/com/gom/timeshare/TimeshareApplication.java
@@ -5,13 +5,10 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.IOException;
-
-import com.google.api.gax.paging.Page;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
 import com.google.common.collect.Lists;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import java.io.FileInputStream;
 import java.util.List;
 
@@ -19,19 +16,24 @@ import java.util.List;
 @SpringBootApplication
 public class TimeshareApplication {
     public static void main(String[] args) throws IOException {
+        SpringApplication.run(TimeshareApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void firebaseInit() throws IOException {
         FirebaseApp firebaseApp = null;
         List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
         if(firebaseApps.stream().noneMatch((e)-> e.getName().equals("push-app"))){
             GoogleCredentials credentials =
-                    GoogleCredentials.fromStream(new FileInputStream("C:\\CODE\\key\\timetable-for-hyeon-firebase-adminsdk-ptefr-dd4e123ae0.json"))
+                    GoogleCredentials.fromStream
+                            (new FileInputStream("C:\\CODE\\key\\timetable-for-hyeon-firebase-adminsdk-ptefr-dd4e123ae0.json"))
                             .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(credentials)
                     .build();
             FirebaseApp.initializeApp(options,"push-app");
+            System.out.println("FirbaseApp 초기화 완료");
         }
-
-        SpringApplication.run(TimeshareApplication.class, args);
     }
 }
